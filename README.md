e20d9860e6ee75fd243f90401fa5ec2c67f693b2
Tomas Aparicio <tomas@aparicio.me>
2016-09-27 22:03:33 +0100

    feat: initial commit, add files

---
+++ README.md
@@ -0,0 +1,29 @@
+# go-is-svg
+
+Tiny package to verify if a given file buffer is an SVG image in Go (golang).
+
+## Installation
+
+```bash
+go get -u github.com/h2non/go-is-svg
+```
+
+## Example
+
+```go
+package main
+
+
+import (
+	"io/ioutil"
+	"github.com/h2non/go-is-svg"
+)
+
+func main() {
+  // TODO
+}
+```
+
+## License
+
+MIT - Tomas Aparicio
---
+++ svg_test.go
@@ -0,0 +1,29 @@
+package issvg
+
+import (
+	"testing"
+)
+
+func TestDeterminateImageType(t *testing.T) {
+	cases := []struct {
+		buf     []byte
+		matches bool
+	}{
+		{[]byte{0}, false},
+		{[]byte{0x00, 0x01, 0x03, 0x04}, false},
+		{[]byte("<svg></svg>"), true},
+		{[]byte(`<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>`), true},
+		{[]byte(`<?xml version="1.0" encoding="utf-8"?>
+						 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1 Basic//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic.dtd">
+						 <svg version="1.1" baseProfile="basic" id="svg2" xmlns:svg="http://www.w3.org/2000/svg" viewBox="0 0 900 900" xml:space="preserve">
+						 	<path id="path482" fill="none" d="M184.013,144.428"/>
+						 	<path id="path6" fill="#FFFFFF" stroke="#000000" stroke-width="0.172" d="917-66.752-80.957C40.928,326.18,72.326,313.197,108.956,403.826z"/>
+						 </svg>`), true},
+	}
+
+	for _, test := range cases {
+		if IsSVG(test.buf) != test.matches {
+			t.Fatalf("Invalid SVG type: %s", test.buf)
+		}
+	}
+}
